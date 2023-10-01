4f3e5c2c918f68b2b91d4a6e0a3dbaf7c01e4632
Eghasbp <eghaxperia@gmail.com>
2023-10-01 16:07:21 +0700

    tambahan

---
+++ src/components/layout/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200 h-10 w-full'>Footer</footer>
+  )
+}
+
+export default Footer
---
+++ src/components/layout/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-slate-200 h-10 w-full'>Header</header>
+  )
+}
+
+export default Header
