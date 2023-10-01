86bb11fbb26f999bae0e8ba0b1ecc16159259cc7
Eghasbp <eghaxperia@gmail.com>
2023-10-01 21:25:45 +0700

    tambahan3

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,5 +1,7 @@
-import Layout from '@/components/layout'
+import Image from 'next/image'
 import React from 'react'
+
+import Layout from '@/components/layout'
 
 const Contact = () => {
   return (
@@ -8,8 +10,9 @@ const Contact = () => {
       <button className='bg-blue-300 px-4 py-2 rounded-lg'>
         Hallo button
       </button>
+      <Image src='/public/svg/vercel.svg' alt='ini adalah gambar'/>
     </Layout>
   )
 }
 
-export default Contact
+export default Contact 
